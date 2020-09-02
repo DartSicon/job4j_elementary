a9a0e9bafc8c5e038e0ae12efab5cdf256898251
DartsiCon <cursedkun@gmail.com>
2020-09-02 10:13:52 +0300

    разбивка тернарных операторов [#350853]

--- src/ru/job4j/condition/MultiMax.java
+++ src/ru/job4j/condition/MultiMax.java
@@ -2,6 +2,7 @@ package ru.job4j.condition;
 
 public class MultiMax {
     public static int max(int first, int second, int third) {
-        return (first > second ? (first > third ? first : third) : (second > third ? second : third));
+        int max = first > second ? first : second;
+        return max > third ? max : third;
     }
 }
